d806c26b07423bb828f68e3807d585ab3bf552c6
Jasmeet Singh <thejasmeet.singh88629@gmail.com>
2022-04-16 20:27:06 +0530

    Added Models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,46 @@
+import uuid
+
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    id = models.UUIDField(default=uuid.uuid4(), unique=True, primary_key=True, editable=False)
+    email = models.EmailField(_("email address"), unique=True)
+    username = None
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+
+
+class HomePrice(models.Model):
+    # Base Fields
+    id = models.UUIDField(default=uuid.uuid4(), unique=True, primary_key=True, editable=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+    modified_on = models.DateTimeField(auto_now=True)
+
+    # CSV Fields
+    date_time = models.DateTimeField()
+    price = models.DecimalField(max_digits=9, decimal_places=2)
+    bedrooms = models.FloatField()
+    sqft_living = models.PositiveIntegerField()
+    sqft_lot = models.PositiveIntegerField()
+    floors = models.FloatField()
+    waterfront = models.PositiveIntegerField()
+    view = models.PositiveIntegerField()
+    condition = models.PositiveIntegerField()
+    sqft_above = models.PositiveIntegerField()
+    sqft_basement = models.PositiveIntegerField()
+    yr_built = models.PositiveIntegerField()
+    yr_renovated = models.PositiveIntegerField()
+    street = models.CharField(max_length=255)
+    city = models.CharField(max_length=100)
+    statezip = models.CharField(max_length=25)
+    country = models.CharField(max_length=10)
+
+    class Meta:
+        ordering = ('-created_on',)
--- home_price/settings.py
+++ home_price/settings.py
@@ -10,7 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^!09grz8^xn+f(q7=s#q5he-tz*=6fjfm0*=gx(b3i*_#gkk=@'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -119,7 +124,4 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'api.User'
